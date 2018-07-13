1e20abb987bfdd193bc9dd34d46d2c53c61d60eb
vgutierrez <vgutierrez@ctisoluciones.com>
2018-07-13 08:29:21 +0200

    Added enlarged card and animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,13 +3,39 @@ import { connect } from 'react-redux';
 import '../styles/card.css';
 
 class Card extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      extraClass: '',
+      enlarged: false,
+    };
+  }
+
   render() {
+    const enlarged = this.state.enlarged;
+
     return (
-      <section className='card'>
-        <h1>{this.props.article.title}</h1>
-        <h2>{this.props.article.author}</h2>
-        <p>{this.props.article.excerpt}</p>
-      </section>
+      enlarged ?
+        <section 
+          className='enlarged-card-background'
+          onClick={() => this.setState({ ...this.state, enlarged: false })}>
+          <section className={`card enlarged`}>
+            <h1>{this.props.article.title}</h1>
+            <h2>{this.props.article.author}</h2>
+            <p>{this.props.article.excerpt}</p>
+          </section>
+        </section>
+        :
+        <section 
+          className={`card ${this.state.extraClass}`} 
+          onMouseEnter={() => this.setState({ ...this.state, extraClass: 'with-shadow' })}
+          onMouseLeave={() => this.setState({ ...this.state, extraClass: ''})}
+          onClick={() => this.setState({ ...this.state, extraClass: '', enlarged: true })} >
+          <h1>{this.props.article.title}</h1>
+          <h2>{this.props.article.author}</h2>
+          <p>{this.props.article.excerpt}</p>
+        </section>
     );
   }
 }
